1a2ead917880605c11b4116602cb4c8beb44a681
3Harang <jungjaeseung9475@gmail.com>
2023-05-25 19:42:02 +0900

    Example
    example commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import { useEffect, useState } from "react";
+import Post from "./components/Post";
+
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [datas, setDatas] = useState([]);
+  const getDatas = async () => {
+    const dataJson = await (await fetch("http://localhost:8080/posts")).json();
+    //console.log(dataJson);
+    setDatas(dataJson);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    getDatas();
+  }, []);
   return (
     <div>
-      <span>초기 설정 완료!!!!!</span>
+      {isLoading ? (
+        <div>
+          <h1>Loading...</h1>
+        </div>
+      ) : (
+        <div>
+          {datas.map((data) => (
+            <Post
+              key={data.no}
+              creator={data.creator}
+              title={data.title}
+              text={data.text}
+              likecount={data.likecount}
+              timestamp={data.timestamp}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
